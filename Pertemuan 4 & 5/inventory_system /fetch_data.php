a7a4bdce63933a8a8803dcc25d7ab10c836c4644
Keishaadb <kadibahasna@gmail.com>
2025-04-13 17:11:05 +0700

    Delete Pertemuan 4 & 5/inventory_system /fetch_data.php

--- Pertemuan 4 & 5/inventory_system /fetch_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-include 'config.php';
-
-$stmt = $pdo->query("SELECT * FROM items ORDER BY created_at DESC");
-$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($items);
-?>
